a3a155f29906b1daed274559b19b40735b8e5f97
Spiriax <spiriax@gmail.com>
2026-05-31 16:12:16 +0200

    Removed test code and added actual UI code

--- index.js
+++ index.js
@@ -5,28 +5,65 @@ jQuery(() => {
 
     console.log("Found panels:", panels.length);
 
-    panels.forEach((panel, index) => {
-        console.log(`Panel ${index}:`, panel);
+    const panel = panels[16];
 
-        const div = document.createElement('div');
+    if (!panel) {
+        console.log("Natural Extended panel not found");
+        return;
+    }
 
-        div.innerHTML = `
-            <div
-                id="natural-extended-test-${index}"
-                style="
-                    background:red;
-                    color:white;
-                    font-size:30px;
-                    padding:20px;
-                    margin:20px;
-                    border:5px solid yellow;
-                ">
-                NATURAL EXTENDED TEST (${index})
-            </div>
-        `;
+    const div = document.createElement('div');
 
-        panel.appendChild(div);
-    });
+    div.innerHTML = `
+    <div id="natural-extended-panel" style="margin-top:10px;">
+
+        <div class="inline-drawer-header">
+            Natural Extended Settings
+        </div>
+
+        <label>
+            <input type="checkbox" id="ne-enabled">
+            Enable for this group
+        </label>
+
+        <p style="opacity:0.7; margin-top:5px;">
+            Group Reply Strategy will automatically be set to Manual while Natural Extended is enabled.
+        </p>
+
+        <hr>
+
+        <div style="margin-top:10px;">
+            <label>
+                Words that make everyone respond
+            </label>
+            <br>
+            <input
+                type="text"
+                id="ne-everyone"
+                style="width:100%;"
+            >
+        </div>
+
+        <hr>
+
+        <div style="margin-top:10px;">
+            <label>
+                Maximum Triggered Characters
+            </label>
+            <br>
+            <input
+                type="number"
+                id="ne-max-mentions"
+                min="0"
+                value="1"
+                style="width:80px;"
+            >
+        </div>
+
+    </div>
+    `;
+
+    panel.appendChild(div);
 
     console.log("Natural Extended injected");
 });
