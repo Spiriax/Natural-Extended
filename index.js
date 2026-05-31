a7ca34440279dddd32882b73bc1d3921ca5a83e9
Spiriax <spiriax@gmail.com>
2026-05-31 14:49:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+
+context.chat; // Chat log - MUTABLE
+context.characters; // Character list
+context.characterId; // Index of the current character
+context.groups; // Group list
+context.groupId; // ID of the current group
+// And many more...
+
+jQuery(() => {
+    const context = SillyTavern.getContext();
+    
+    console.log("Natural Extended loaded");
+
+    const panel = document.querySelector(
+        '.inline-drawer.wide100p.flexFlowColumn'
+    );
+
+    if (!panel) {
+        console.log("Panel not found");
+        return;
+    }
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+        <div id="natural-extended-test"
+             style="
+                margin:10px;
+                padding:10px;
+                border:1px solid #666;
+             ">
+            <h3>Natural Extended</h3>
+            <label>
+                <input type="checkbox">
+                Enable for this group
+            </label>
+        </div>
+    `;
+
+    panel.appendChild(div);
+
+    console.log("Natural Extended injected");
+});
