aef6152ef545ea7d4f7d2b73f17154fd11232686
Spiriax <spiriax@gmail.com>
2026-06-03 05:48:39 +0200

    Test of moving UI again

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ jQuery(() => {
         
     const panels = document.querySelectorAll('.inline-drawer-content');
 
-    const panel = panels[19];
+    const panel = panels[15];
     
     if (!panel) {
         console.log("[ 🦜 Natural Extended ] ❓ Could not find group panel!❓");
